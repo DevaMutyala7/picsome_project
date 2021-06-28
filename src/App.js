2b2d839c7703d7361349480fcdccfdab1feab353
Deva <82375896+DevaMutyala7@users.noreply.github.com>
2021-06-28 20:21:55 +0530

    lazy loading implementation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react'
-import Signup from './Components/Signup'
-import Login from './Components/Login'
-import Dashboard from './Components/Dashboard'
+import React, { lazy, Suspense } from 'react'
 import ProtectedRoute from './Components/ProtectedRoute'
-import Cart from './Components/Pages/Cart'
 import {AuthProvider} from '../src/Context/AuthContext.js'
 import './App.css'
 import {HashRouter as Router, Route, Switch} from 'react-router-dom'
 
+let Signup =lazy(()=> import('./Components/Signup')) 
+let Login =lazy(()=> import('./Components/Login'))
+let Dashboard =lazy(()=> import('./Components/Dashboard')) 
+let Cart =lazy(()=> import('./Components/Pages/Cart'))
+
 function App() {
   return (
         <Router>
-              <AuthProvider>
-                  <div className="main">
-                        <Switch>
-                              <ProtectedRoute exact path="/" component={Dashboard} />
-                              <ProtectedRoute path="/cart" component={Cart} />
-                              <Route path="/signup"  component = {Signup}/>
-                              <Route path="/login" component={Login} />
-                        </Switch>
-                  </div>
-            </AuthProvider>
+              <Suspense fallback={<h1>Loading....</h1>}>
+                  <AuthProvider>
+                        <div className="main">
+                              <Switch>
+                                    <ProtectedRoute exact path="/" component={Dashboard} />
+                                    <ProtectedRoute path="/cart" component={Cart} />
+                                    <Route path="/signup"  component = {Signup}/>
+                                    <Route path="/login" component={Login} />
+                              </Switch>
+                        </div>
+                  </AuthProvider>
+              </Suspense>
         </Router>
   );
 }
